34933647ce5bdd5e30fad6e6547bc0ebbeb0e466
lyoko¡ <38739487+lyoko98s@users.noreply.github.com>
2021-04-12 00:37:07 +0200

    Update blacklistdb.py

--- database/blacklistdb.py
+++ database/blacklistdb.py
@@ -44,7 +44,8 @@ async def num_blacklist_triggers_chat(chat_id):
 
 
 async def is_blacklist_in_db(chat_id, trigger):
-    r = [u async for u in blacklist.find({"chat_id": chat_id})]
+    r = blacklist.distinct('trigger')
+#    r = [u async for u in blacklist.find({"chat_id": chat_id})]
     for x in r:
         if trigger.find("r[x]") >= 0:
             return True
