429d8326bab12c02df6b3153b8422e9f0e38c4c9
lyoko¡ <38739487+lyoko98s@users.noreply.github.com>
2021-04-12 02:48:36 +0200

    Update blacklistdb.py

--- database/blacklistdb.py
+++ database/blacklistdb.py
@@ -53,6 +53,22 @@ async def num_blacklist_triggers_chat(chat_id):
 #            break
 #        else:
 #            continue
+
+Class Aiter :
+    def __init__(self, iterable):
+        self.iter_ = iter(iterable)
+  
+    async def __aiter__(self):
+        return self
+  
+    async def __anext__(self):
+        await asyncio .sleep(0)
+        try:
+            object = next(self.iter_)
+        except StopIteration:
+            raise StopAsyncIteration # :-) PEP492 - "To stop iteration __anext__ must raise a StopAsyncIteration exception"
+  
+        return object
             
 async def is_blacklist_in_db(trigger):
     r = blacklist.distinct('trigger')
